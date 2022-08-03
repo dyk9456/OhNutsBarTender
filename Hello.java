fa6451073e7a9bef8f10cb002f260ba4d7b59baf
David Klein <dykflights@gmail.com>
2022-08-03 13:10:36 -0400

    login button click

--- Hello.java
+++ Hello.java
@@ -1,23 +1,31 @@
 
 import java.awt.event.*;
+import java.sql.Array;
 import java.awt.Robot;
 import java.awt.MouseInfo;
 import java.util.concurrent.TimeUnit;
 
 
 public class Hello{
+
+        
     public static void main(String[] args) throws Exception{
+
+        int LoginX = 16;
+        int LoginY = 40;
+
+
         System.out.println("Hello World!");
         
         //starts the program and waits for 3 seconds
         Runtime runTime = Runtime.getRuntime();
         String executablePath = "explorer ";
         Process process = runTime.exec(executablePath); 
-        TimeUnit.SECONDS.sleep(3);
+        TimeUnit.SECONDS.sleep(5);
 
-        //moves mouse
+        //moves mouse to Login button and clicks it
         Robot robot = new Robot();
-        robot.mouseMove(1474, 161);
+        robot.mouseMove(LoginX, LoginY);
         int mask = InputEvent.BUTTON1_DOWN_MASK;
         robot.mousePress( mask );
         robot.mouseRelease(mask);
@@ -26,21 +34,10 @@ public class Hello{
         TimeUnit.SECONDS.sleep(3);
 
         //clicks on the text box
-        robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_CONTROL);
-        robot.keyRelease(KeyEvent.VK_V);  
+        // robot.keyPress(KeyEvent.VK_CONTROL);
+        // robot.keyPress(KeyEvent.VK_V);
+        // robot.keyRelease(KeyEvent.VK_CONTROL);
+        // robot.keyRelease(KeyEvent.VK_V);  
 
-   
-
-
-        // while(true == true) {
-        // TimeUnit.SECONDS.sleep(1/2);
-        // double mouseX = MouseInfo.getPointerInfo().getLocation().getX();
-        // double mouseY = MouseInfo.getPointerInfo().getLocation().getY();
-        // System.out.println("X:" + mouseX);
-        // System.out.println("Y:" + mouseY);
-        // //make sure to import 
-        // }
     }
 }   
